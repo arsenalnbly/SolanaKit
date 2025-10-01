d07ff3b52273aac3f9d7cf468fcf5d4dcbf27ddc
arsenalnbly <asekmad@gmail.com>
2025-10-01 20:03:29 +0200

    Make AccountTransfer hashable

--- Sources/SolscanClient/Models/Account/AccountTransfer.swift
+++ Sources/SolscanClient/Models/Account/AccountTransfer.swift
@@ -5,16 +5,26 @@
 //  Created by arsenal on 25.09.25.
 //
 
-public struct AccountTransfer: Codable {
-      public let block_id: Int
-      public let trans_id: String
-      public let block_time: Int
-      public let time: String
-      public let activity_type: String
-      public let from_address: String
-      public let to_address: String
-      public let token_address: String
-      public let token_decimals: Int
-      public let amount: Int64
-      public let flow: String
-  }
+public struct AccountTransfer: Codable, Hashable {
+    public let block_id: Int
+    public let trans_id: String
+    public let block_time: Int
+    public let time: String
+    public let activity_type: String
+    public let from_address: String
+    public let to_address: String
+    public let token_address: String
+    public let token_decimals: Int
+    public let amount: Int64
+    public let flow: String
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(trans_id)
+        hasher.combine(to_address)
+        hasher.combine(from_address)
+    }
+    
+    public static func == (lhs: AccountTransfer, rhs: AccountTransfer) -> Bool {
+        lhs.trans_id == rhs.trans_id && lhs.to_address == rhs.to_address && lhs.from_address == rhs.from_address
+    }
+}
