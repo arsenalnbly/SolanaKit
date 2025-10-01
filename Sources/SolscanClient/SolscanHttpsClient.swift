eaf0e5eee4cf22b8646c13d0f8ca8bf10df8f5f8
arsenalnbly <asekmad@gmail.com>
2025-10-01 15:01:25 +0200

    debug

--- Sources/SolscanClient/SolscanHttpsClient.swift
+++ Sources/SolscanClient/SolscanHttpsClient.swift
@@ -20,7 +20,7 @@ public final class SolscanHttpsClient {
     
     private func fetch(_ request: URLRequest) async throws -> Data {
         let (data, response) = try await URLSession.shared.data(for: request)
-        print(String(data: data, encoding: .utf8))
+        print("got \(String(data: data, encoding: .utf8)) for \(request.url?.absoluteString)")
         
         guard let httpResponse = response as? HTTPURLResponse,
               (200..<300).contains(httpResponse.statusCode) else {
